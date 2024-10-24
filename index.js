1d678ef7ef98290e9a46f13153688fbba68dd1c8
ImoEazy <imieadams@gmail.com>
2024-10-24 02:09:22 +0200

    added function for velocity calc

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
  * 3. Make these calculations robust such that the calculation does not give an incorrect result, it throws an error to the user if something has gone wrong (parameter used with an incorrect unit of measurement, etc)
  */
 
-// Given Parameters
+ //Given Parameters
 const velocityKmh = 10000; // velocity (km/h) changed const
 const accelerationMps2 = 3; // acceleration (m/s^2) 
 const timeInSeconds = 3600; // seconds (1 hour)
@@ -13,15 +13,26 @@ const initialDistanceKm = 0; // distance (km)
 const remainingFuelKguel = 5000; // remaining fuel (kg)
 const fuelBurnRateKgPerS = 0.5; // fuel burn rate (kg/s)
 
+//function for veocity calc
+//create const calcNewvelocity
+const calcNewVelocity = (InitialVelocity, acceleration, time) => {
+if (typeof initialVelocity !== 'number' || typeof acceleration !== 'number' || typeof time !== 'number') {
+  throw new Error('Invalid input: velocity, acceleration, and time must be numbers.');
+
+}
+return initialVelocity + (acceleration * time);
+
+};
+
 
 const d2 = d + (vel*time) //calcultes new distance
 const rf = fbr*time //calculates remaining fuel
 const vel2 = calcNewVel(acc, vel, time) //calculates new velocity based on acceleration
 
 // Pick up an error with how the function below is called and make it robust to such errors
-calcNewVel = (vel, acc, time) => { 
-  return vel + (acc*time)
-}
+//calcNewVel = (vel, acc, time) => { 
+  //return vel + (acc*time)
+//}
 
 console.log(`Corrected New Velocity: ${vel2} km/h`);
 console.log(`Corrected New Distance: ${d2} km`);
